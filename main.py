34b17fb03c300c8ba877e48b3f236a7f4ac83a74
yoon0915 <mikooyoon@naver.com>
2024-11-19 22:06:44 +0900

    Add Brick break & Snake game

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import os
 pygame.init()
 
 # 화면 설정
-WIDTH, HEIGHT = 400, 300
+WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("게임 실행기")
+pygame.display.set_caption("고전 전자 오락")
 
 # 색상 정의
 WHITE = (255, 255, 255)
@@ -44,9 +44,20 @@ def run_tetris():
 def run_minesweeper():
     subprocess.Popen(["python", os.path.join("game", "Minesweeper.py")])
 
+def run_Brick_break():
+    subprocess.Popen(["python", os.path.join("game", "Brick_break.py")])
+
+def run_Snake():
+    subprocess.Popen(["python", os.path.join("game", "Snake.py")])
+
 # 버튼 생성
-tetris_button = Button(100, 80, 200, 50, "Tetris", run_tetris)
-minesweeper_button = Button(100, 160, 200, 50, "Minesweeper", run_minesweeper)
+tetris_button = Button(100, 50, 200, 50, "Tetris", run_tetris)
+minesweeper_button = Button(100, 120, 200, 50, "Minesweeper", run_minesweeper)
+brick_break_button = Button(100, 190, 200, 50, "Brick Break", run_Brick_break)
+snake_button = Button(100, 260, 200, 50, "Snake", run_Snake)
+
+# 버튼 리스트
+buttons = [tetris_button, minesweeper_button, brick_break_button, snake_button]
 
 # 메인 루프
 running = True
@@ -58,24 +69,20 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
-            tetris_button.check_click(mouse_pos)
-            minesweeper_button.check_click(mouse_pos)
+            for button in buttons:
+                button.check_click(mouse_pos)
     
     # 버튼에 마우스 오버 효과
     mouse_pos = pygame.mouse.get_pos()
-    if tetris_button.rect.collidepoint(mouse_pos):
-        tetris_button.color = BUTTON_HOVER_COLOR
-    else:
-        tetris_button.color = BUTTON_COLOR
-
-    if minesweeper_button.rect.collidepoint(mouse_pos):
-        minesweeper_button.color = BUTTON_HOVER_COLOR
-    else:
-        minesweeper_button.color = BUTTON_COLOR
+    for button in buttons:
+        if button.rect.collidepoint(mouse_pos):
+            button.color = BUTTON_HOVER_COLOR
+        else:
+            button.color = BUTTON_COLOR
 
     # 버튼 그리기
-    tetris_button.draw(screen)
-    minesweeper_button.draw(screen)
+    for button in buttons:
+        button.draw(screen)
 
     pygame.display.flip()
 
